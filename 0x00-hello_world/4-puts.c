18f53cd87bd71aebba51cfe4d819b5f5d51dae72
my name <myemail@gmail.com>
2023-03-16 19:54:04 -0700

    lkj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
-*main - entry poit
-*return: always 0 (success)
-*/
+ * main - input ine with function puts
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
