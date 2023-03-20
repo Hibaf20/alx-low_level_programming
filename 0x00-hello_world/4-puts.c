68c6341e58be6e1437ffeafbd12821f34d8f7ee5
my name <myemail@gmail.com>
2023-03-20 12:56:11 -0700

    fff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - input ine with function puts
- * Return:0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
